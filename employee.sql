e009dc4cb4aeeb8b933a46e17ae57d5232173b88
bekar-dev <birajitdebbarma@gmail.com>
2022-08-04 16:28:08 +0530

    Update employee.sql

--- employee.sql
+++ employee.sql
@@ -6,3 +6,15 @@ CREATE table employee(
     employee_address text,
     PRIMARY KEY(id)
 )
+
+INSERT INTO `employee`(`id`, `employee_name`, `employee_duration`, `employee_status`, `employee_address`) VALUES ('1','BIRAJIT DEBBARMA','6','ACTIVE','DELHI, INDIA');
+
+INSERT INTO `employee`(`id`, `employee_name`, `employee_duration`, `employee_status`, `employee_address`) VALUES ('2','ANISH DEBBARMA','5','ACTIVE','GOA, INDIA');
+
+INSERT INTO `employee`(`id`, `employee_name`, `employee_duration`, `employee_status`, `employee_address`) VALUES ('3','PRASHANT DEBBARMA','7','NOT ACTIVE','DELHI, INDIA');
+
+INSERT INTO `employee`(`id`, `employee_name`, `employee_duration`, `employee_status`, `employee_address`) VALUES ('4','HAMARI JAMATIA','4','ACTIVE','UP, INDIA');
+
+INSERT INTO `employee`(`id`, `employee_name`, `employee_duration`, `employee_status`, `employee_address`) VALUES ('5','KINGSHUK DEBBARMA','6','ACTIVE','BIHAR, INDIA');
+
+INSERT INTO `employee`(`id`, `employee_name`, `employee_duration`, `employee_status`, `employee_address`) VALUES ('6','HITESH DEBBARMA','3','ACTIVE','GUWAHATI, INDIA');
